292d2509a6fcdfbdfe802ba57087990b21874fe3
athiragithubworld <athirahrms@gmail.com>
2023-07-18 00:50:54 +0530

    App js - Task 15

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthentication);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
